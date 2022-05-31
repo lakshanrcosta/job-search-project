9f4e5497bb0d531c18d1125abd778421bae358ef
Lakshan Costa <lakshan.costa@silo.ai>
2022-05-31 18:33:24 +0300

    Added main nav component

---
+++ src/components/MainNav.vue
@@ -0,0 +1,47 @@
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 w-full h-16 bg-white">
+      <div
+        class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
+      >
+        <a :href="url" class="flex items-center h-full text-xl">{{
+          company
+        }}</a>
+        <nav class="h-full ml-12">
+          <ul class="flex h-full p-0 m-0 list-none">
+            <li
+              v-for="(menuItem, index) in menuItems"
+              :key="index"
+              class="h-full first:ml-0 ml-10"
+              data-test="main-nav-list-item"
+            >
+              <a href="" class="flex items-center h-full py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+  data() {
+    return {
+      company: "Bobo Careers",
+      url: "",
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at Bobo Corp",
+        "How we hire",
+        "Students",
+        "Jobs",
+      ],
+    };
+  },
+};
+</script>
