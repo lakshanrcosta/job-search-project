c66c60a4642aad59e90697f4ae8c7766062f0169
Lakshan Costa <lakshancst@yahoo.com>
2022-06-01 01:55:14 +0300

    Added login action button and profile image components

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -21,14 +21,25 @@
             </li>
           </ul>
         </nav>
+        <div class="flex items-center h-full ml-auto">
+          <profile-image v-if="isLoggedIn" />
+          <action-button v-else />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from "@/components/ActionButton.vue";
+import ProfileImage from "@/components/ProfileImage.vue";
+
 export default {
   name: "MainNav",
+  components: {
+    ActionButton,
+    ProfileImage,
+  },
   data() {
     return {
       company: "Bobo Careers",
@@ -41,6 +52,7 @@ export default {
         "Students",
         "Jobs",
       ],
+      isLoggedIn: false,
     };
   },
 };
